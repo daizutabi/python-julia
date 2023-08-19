11fe1d7a439e58d8d1ef3503dfc157a8443159e7
daizu <daizutabi@gmail.com>
2023-08-19 11:32:15 +0900

    add Genie

--- julia/add.jl
+++ julia/add.jl
@@ -2,18 +2,22 @@ using Pkg
 
 include("env.jl")
 
-@assert ENV["JULIA_CONDAPKG_BACKEND"] == "Null"
-@assert endswith(ENV["JULIA_PYTHONCALL_EXE"], joinpath(read_envname(), "python.exe"))
-
-const CONFIG_DIR = joinpath(DEPOT_PATH[1], "config")
-
-isdir(CONFIG_DIR) || mkdir(CONFIG_DIR)
+envs = ["JULIA_CONDAPKG_BACKEND", "JULIA_PYTHONCALL_EXE"]
+@assert ENV[envs[1]] == "Null"
+@assert endswith(ENV[envs[2]], joinpath(read_envname(), "python.exe"))
+for env in envs
+    println("$env: $(ENV[env])")
+end
 
 open("pkg.txt") do f
     Pkg.add(readlines(f))
 end
 
 Pkg.precompile()
+
+const CONFIG_DIR = joinpath(DEPOT_PATH[1], "config")
+
+isdir(CONFIG_DIR) || mkdir(CONFIG_DIR)
 
 function write_startup(path, src)
     open(joinpath(CONFIG_DIR, path), "w") do f
